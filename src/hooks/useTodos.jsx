9dd90f82878d0c86cafe5dec09021084b7c80a15
DariochinITS <dariochin28@gmail.com>
2024-07-04 10:13:17 +0200

    aggiunta struttura progetto

---
+++ src/hooks/useTodos.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+const useTodos = initialTodos => {
+  const [todos, setTodos] = useState(initialTodos);
+
+  const addTodo = todo => {
+    setTodos([...todos, todo]);
+  };
+
+  const toggleTodo = id => {
+    setTodos(
+      todos.map(todo =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  return {
+    todos,
+    addTodo,
+    toggleTodo
+  };
+};
+
+export default useTodos;
